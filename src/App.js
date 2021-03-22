601d871bc8a311b4453256e9bfc7ab68c7b0a00c
chris097 <chrisfidel.international@gmail.com>
2021-03-22 06:44:36 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Reg } from "./components/register";
+
+
+function App() {
+  return (
+     <>
+        <Reg />
+     </>
+  );
+}
+
+export default App;
---
+++ src/components/register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export const Reg = () => {
+    return(
+        <>
+            <div>
+                <div class="text-xl">Hello work</div>
+            </div>
+        </>
+    )
+}
